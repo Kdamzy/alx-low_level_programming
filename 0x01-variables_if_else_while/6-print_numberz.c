f2d6c441dd9eccbfde5774a60e011ff1fe2700a2
Kdamzy <kolajidedamilare@gmail.com>
2023-08-12 04:46:45 -0700

    number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,11 @@
   */
 int main(void)
 {
-	int number = 0;
+	int n;
 
-	while (number < 10
+	for (n = '0'; n <= '9'; n++)
 	{
-		putchar(number + '0');
-		number++;
+		putchar(n);
 	}
 	putchar('\n');
 	return (0);
