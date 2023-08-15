a289dbbc69b13e2057b976319ce15a803d11447e
Kdamzy <kolajidedamilare@gmail.com>
2023-08-15 04:23:57 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints "_putchar" followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_putchar();
+	putchar('\n');
+
+	return (0);
+}
