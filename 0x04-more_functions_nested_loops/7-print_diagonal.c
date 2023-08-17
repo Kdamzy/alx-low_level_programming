b117df1bfda581bc3704e85bea7f2ea79e9cc621
Kdamzy <kolajidedamilare@gmail.com>
2023-08-17 05:10:08 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,13 +3,14 @@
 /**
  * print_diagonal - Prints a diagonal line of backslashes with indentation
  * @n: The number of times the character '\\' should be printed
+ * Return: void
  */
 void print_diagonal(int n)
 {
 	int i, j;
 
 	if (n <= 0)
-		_putchar('\n');}
+		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < row; j++)
