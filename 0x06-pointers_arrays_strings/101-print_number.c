16104e88eaec5ae70c3c9e0c03b8b6e98aed6616
Kdamzy <kolajidedamilare@gmail.com>
2023-08-24 07:45:33 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,30 +7,14 @@
 
 void print_number(int n)
 {
-	int a, b, c, d;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	d = n % 10;
-	c = ((n / 10) % 10);
-	b = ((n / 100) % 10);
-	a = ((n / 1000) % 10);
+	if (n / 10 != 0)
+		print_number(n / 10);
 
-	if (a != 0)
-		_putchar(a + '0');
-	if (a != 0 || b != 0)
-		_putchar(b + '0');
-	if (a != 0 || b != 0 || c != 0)
-		_putchar(c + '0');
-	_putchar(d + '0');
+	_putchar((n % 10) + '0');
 }
