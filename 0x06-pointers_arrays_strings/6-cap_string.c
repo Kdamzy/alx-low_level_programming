fa9afe7bb57a22f88475ee51770f4d3a2b4ec700
Kdamzy <kolajidedamilare@gmail.com>
2023-08-24 05:41:09 -0700

    convert from lowercase to uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,48 +2,37 @@
 
 /**
  * cap_string - Capitalizes all words of a string.
- * @str: The string to modify.
- * Return: A pointer to the modified string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
  */
 
 char *cap_string(char *str)
 {
-	int i = 0;
+	int index = 0;
 
-	/* If the first character is lowercase, convert it to uppercase */
-	if (str[i] >= 'a' && str[i] <= 'z')
+	while (str[index])
 	{
-		str[i] = str[i] - 'a' + 'A';
-	}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-	/* Loop through the string until null terminator is encountered */
-	while (str[i] != '\0')
-	{
-		/* Check if the current character is a separator */
-		if (str[i-1] == ' ' || str[i-1] == '\t' || str[i-1] == '\n' ||
-				str[i-1] == ',' || str[i-1] == ';' || str[i-1] == '.' ||
-				str[i-1] == '!' || str[i-1] == '?' || str[i-1] == '"' ||
-				str[i-1] == '(' || str[i-1] == ')' || str[i-1] == '{' || str[i-1] == '}')
-		{
-			/* Move to the next character */
-			i++;
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
 
-			/* If the next character is lowercase, convert it to uppercase */
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 'a' + 'A';
-			}
-		}
-		else
-		{
-			i++;
-
-			/* If the next character is uppercase, convert it to lowercase */
-			if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] = str[i] + 'a' - 'A';
-			}
-		}
+		index++;
 	}
 
 	return (str);
