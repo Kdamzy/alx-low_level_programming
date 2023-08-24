e0c95b59510c80be5e99965e795ea8923c55dd9f
Kdamzy <kolajidedamilare@gmail.com>
2023-08-24 03:03:02 -0700

    convert lower to upper

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to modify.
+ * Return: A pointer to the modified string.
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	/* If the first character is lowercase, convert it to uppercase */
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 'a' + 'A';
+	}
+
+	/* Loop through the string until null terminator is encountered */
+	while (str[i] != '\0')
+	{
+		/* Check if the current character is a separator */
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
+		{
+			/* Move to the next character */
+			i++;
+
+			/* If the next character is lowercase, convert it to uppercase */
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 'a' + 'A';
+			}
+		}
+		else
+		{
+			/* Move to the next character */
+			i++;
+
+			/* If the next character is uppercase, convert it to lowercase */
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 'a' - 'A';
+			}
+		}
+	}
+
+	/* Return the modified string */
+	return (str);
+}
