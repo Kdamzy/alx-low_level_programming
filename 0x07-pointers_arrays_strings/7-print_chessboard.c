18cdffea37b15eb3a7d148510fe2b59cce234e7c
Kdamzy <kolajidedamilare@gmail.com>
2023-08-28 06:55:48 -0700

    print cheeseboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,9 +12,9 @@ void print_chessboard(char (*a)[8])
 	int i;
 	int n;
 
-	for (int i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (int n = 0; n < 8; n++)
+		for (n = 0; n < 8; n++)
 		{
 			_putchar(a[i][n]);
 		}
