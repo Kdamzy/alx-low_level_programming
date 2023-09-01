5ab9bb32b9eca3637fbeedd8982b1b27ee029b98
Kdamzy <kolajidedamilare@gmail.com>
2023-09-01 13:06:45 -0700

    program that multiply two numbers" " "

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,7 +7,6 @@
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
@@ -30,12 +29,10 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
@@ -50,26 +47,24 @@ int _atoi(char *s)
 }
 
 /**
- * main - Multiplies two numbers and prints the result.
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the arguments.
- *
- * Return: 0 on success, 1 on error.
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int result, num1, num2;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-
 		return (1);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);
