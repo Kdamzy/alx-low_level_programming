32b2a7fbf6c8c59270e65a58c17b1dfcf85f082e
Kdamzy <kolajidedamilare@gmail.com>
2023-09-01 12:30:52 -0700

    program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -30,10 +30,12 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
+
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
+
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
@@ -68,10 +70,9 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-
 	result = num1 * num2;
 
 	printf("%d\n", result);
 
 	return (0);
-}i
+}
