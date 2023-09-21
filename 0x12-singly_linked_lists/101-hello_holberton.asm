4c5ee574ec8d0a2b96e34354c2fb76c464cf166c
Kdamzy <kolajidedamilare@gmail.com>
2023-09-21 15:15:12 -0700

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,9 @@
-/* Declare the main function as global */
 global   main
-
-/* Declare that we will use the printf function from an external source */
 	  extern    printf
 main:
-
-/* Set the format string address as the first argument (EDI) */
 	  mov   edi, format
-
-/* Clear the EAX register (return value) */
 	  xor   eax, eax
-
-/* Call the printf function */
 	  call  printf
-
-/* Set the return value to 0 (success) */
 	  mov   eax, 0
-
-/* Return from the main function */
 	  ret
-
-/* Define the format string */
 format: db `Hello, Holberton\n`,0
