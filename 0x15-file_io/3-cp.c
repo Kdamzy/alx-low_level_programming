634ab8311f07ff15987e11bb2848aa756da48c16
Kdamzy <kolajidedamilare@gmail.com>
2023-10-02 09:37:02 -0700

    a  program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,6 +16,82 @@ void error(int code, const char *message)
 }
 
 /**
+ * open_f - Open a file with specified flags and mode.
+ * @filename: The name of the file to open.
+ * @flags: The flags to use when opening the file.
+ * @mode: The file mode to set (used for creation).
+ *
+ * Return: The file descriptor on success
+ */
+int open_f(const char *filename, int flags, mode_t mode)
+{
+	int fd;
+
+	fd = open(filename, flags, mode);
+	if (fd == -1)
+	{
+		error(99, "Can't open file");
+	}
+	return (fd);
+}
+
+/**
+ * create_buffer - Dynamically allocate memory for a buffer.
+ *
+ * Return: A pointer to the allocated buffer on success
+ */
+char *create_buffer()
+{
+	char *buffer = malloc(BUFFER_SIZE);
+
+	if (buffer == NULL)
+	{
+		error(99, "Failed to allocate memory");
+	}
+	return (buffer);
+}
+
+/**
+ * copy - Copy the content of one file to another.
+ * @source: The path to the source file to be copied.
+ * @destination: The path to the destination file
+ *
+ * Return: No return value. Exits with an error code on failure.
+ */
+void copy(const char *source, const char *destination)
+{
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+
+	fd_from = open_f(source, O_RDONLY, 0);
+	fd_to = open_f(destination, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	buffer = create_buffer();
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			free(buffer);
+			error(99, "Can't write to file");
+		}
+	}
+	if (bytes_read == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		free(buffer);
+		error(98, "Can't read from file");
+	}
+	close(fd_from);
+	close(fd_to);
+	free(buffer);
+}
+
+/**
  * main - Copy the content of one file to another.
  * @argc: The number of arguments.
  * @argv: An array of command-line arguments.
@@ -24,43 +100,14 @@ void error(int code, const char *message)
  */
 int main(int argc, char *argv[])
 {
-	int from, to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+	const char *source = argv[1];
+	const char *destination = argv[2];
 
 	if (argc != 3)
 	{
 		error(97, "Usage: cp file_from file_to");
 	}
-	from = open(argv[1], O_RDONLY);
-	if (from == -1)
-	{
-		error(98, "Can't read from file");
-	}
-	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (to == -1)
-	{
-		error(99, "Can't write to file");
-	}
-	while ((bytes_read = read(from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			error(99, "Can't write to file");
-		}
-	}
-	if (bytes_read == -1)
-	{
-		error(98, "Can't read from file");
-	}
-	if (close(from) == -1)
-	{
-		error(100, "Can't close fd");
-	}
-	if (close(to) == -1)
-	{
-		error(100, "Can't close fd");
-	}
+	copy(source, destination);
+
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,8 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void error(int code, const char *message);
 int main(int argc, char *argv[]);
+void copy(const char *source, const char *destination);
+char *create_buffer(void);
+int open_f(const char *filename, int flags, mode_t mode);
 
 #endif
